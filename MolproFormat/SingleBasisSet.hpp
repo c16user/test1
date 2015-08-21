fc369d93e3d417069d5cd435ba8e3a48d06aab3c
Dima <dmchestnykh@yandex.ru>
2015-08-22 00:55:38 +0300

    Улучшения в выводе  бызиса в формате Molpro. Правда не работает новая огромная функция-замены полного названия элемента на сокращенное

--- MolproFormat/SingleBasisSet.hpp
+++ MolproFormat/SingleBasisSet.hpp
@@ -7,6 +7,7 @@
 #include<fstream>
 #include<string>
 #include<iterator>
+#include<stdio.h>
 #include"GamessFormat.hpp"
 #include"MolproFormat.hpp"
 
@@ -163,7 +164,6 @@ bool SingleBasisSet<T>::importBasisSetGamessFormat( const char * fileName){
 	return true;
 }
 
-//template<typename T>
 char shellName(int u){
 	if(u==0) return 's';
 	if(u==1) return 'p';
@@ -180,6 +180,113 @@ char shellName(int u){
 	return '1';
 }
 
+string elementSymbol(string str){
+	if(str=="HYDROGEN") return "H";
+	if(str=="HELIUM") return "HE";
+	if(str=="LITHIUM") return "LI";
+	if(str=="BERYLLIUM" || str=="GLUCINIUM") return "BE";
+	if(str=="BORON") return "B";
+	if(str=="CARBON") return "C";
+	if(str=="NITROGEN") return "N";
+	if(str=="OXYGEN") return "O";
+	if(str=="FLUORINE") return "F";
+	if(str=="NEON") return "NE";
+	if(str=="NATRIUM" || str=="SODIUM") return "NA";
+	if(str=="MAGNESIUM") return "MG";
+	if(str=="ALUMINIUM") return "AL";
+	if(str=="SILICON") return "SI";
+	if(str=="PHOSPHOROUS") return "P";
+	if(str=="SULFUR") return "S";
+	if(str=="CHLORINE") return "CL";
+	if(str=="ARGON") return "AR";
+	if(str=="POTASSIUM") return "K";
+	if(str=="CALCIUM") return "CA";
+	if(str=="SCANDIUM") return "SC";
+	if(str=="TITANIUM") return "TI";
+	if(str=="VANADIUM") return "V";
+	if(str=="CHROMIUM") return "CR";
+	if(str=="MANGANESE") return "MN";
+	if(str=="IRON") return "FE";
+	if(str=="COBALT") return "CO";
+	if(str=="NICKEL") return "NI";
+	if(str=="COPPER") return "CU";
+	if(str=="ZINC") return "ZN";
+	if(str=="GALLIUM") return "GA";
+	if(str=="GERMANIUM") return "GE";
+	if(str=="ARSENIC") return "AS";
+	if(str=="SELENIUM") return "SE";
+	if(str=="BROMINE") return "BR";
+	if(str=="KRYPTON") return "KR";
+	if(str=="RUBIDIUM") return "RB";
+	if(str=="STRONTIUM") return "SR";
+	if(str=="YTTRIUM") return "Y";
+	if(str=="NIOBIUM") return "NB";
+	if(str=="MOLYBDENUM") return "MO";
+	if(str=="TECHNETIUM") return "H";
+	if(str=="RUTHENIUM") return "RU";
+	if(str=="RHODIUM") return "RH";
+	if(str=="PALLADIUM") return "PD";
+	if(str=="ARGENTUM") return "AG";
+	if(str=="CADMIUM") return "CD";
+	if(str=="INDIUM") return "IN";
+	if(str=="TIN") return "SN";
+	if(str=="STIBIUM" || str=="ANTIMONY") return "SB";
+	if(str=="TELLURIUM") return "TE";
+	if(str=="IODINE") return "I";
+	if(str=="XENON") return "XE";
+	if(str=="CAESIUM") return "CS";
+	if(str=="BARIUM") return "BA";
+	if(str=="LANTHANUM") return "LA";
+	if(str=="CERIUM") return "CE";
+	if(str=="PRASEODYMIUM") return "PR";
+	if(str=="NEODYMIUM") return "ND";
+	if(str=="PROMETHIUM") return "PM";
+	if(str=="SAMARIUM") return "SM";
+	if(str=="EUROPIUM") return "EU";
+	if(str=="GADOLINIUM") return "GD";
+	if(str=="TERBIUM") return "TB";
+	if(str=="DYSPROSIUM") return "DY";
+	if(str=="HOLMIUM") return "HO";
+	if(str=="ERBIUM") return "ER";
+	if(str=="THULIUM") return "TM";
+	if(str=="YTTERBIUM") return "YB";
+	if(str=="LUTECIUM") return "LU";
+	if(str=="HAFNIUM") return "HF";
+	if(str=="TANTALUM") return "TA";
+	if(str=="TUNGSTEN") return "W";
+	if(str=="RHENIUM") return "RE";
+	if(str=="OSMIUM") return "OS";
+	if(str=="IRIDIUM") return "IR";
+	if(str=="PLATINUM") return "PT";
+	if(str=="GOLD") return "AU";
+	if(str=="MERCURY" || str=="QUICKSILVER") return "HG";
+	if(str=="THALLIUM") return "TL";
+	if(str=="LEAD") return "PB";
+	if(str=="BISMUTH") return "BI";
+	if(str=="POLONIUM") return "PO";
+	if(str=="ASTATINE") return "AT";
+	if(str=="RADON") return "RN";
+	if(str=="FRANCIUM") return "FR";
+	if(str=="RADIUM") return "RA";
+	if(str=="ACTINIUM") return "AC";
+	if(str=="THORIUM") return "TH";
+	if(str=="PROTACTINIUM") return "PA";
+	if(str=="URANIUM") return "U";
+	if(str=="PLUTONIUM") return "NP";
+	if(str=="AMERICIUM") return "PU";
+	if(str=="CURIUM") return "CM";
+	if(str=="BERKELIUM") return "BK";
+	if(str=="CALIFORNIUM") return "CF";
+	if(str=="EINSTEINIUM") return "ES";
+	if(str=="FERMIUM") return "FM";
+	if(str=="MENDELEVIUM") return "MD";
+	if(str=="NOBELIUM") return "NO";
+	if(str=="LAWRENCIUM") return "LR";
+	if(str=="RUTHERFORDIUM") return "RF";
+	return "EN";
+
+}
+
 template< typename T >
 bool SingleBasisSet<T>::exportBasisSetMolproFormat( const char * fileName) const{
 		ofstream out("outputFile");
@@ -189,22 +296,27 @@ bool SingleBasisSet<T>::exportBasisSetMolproFormat( const char * fileName) const
 		unsgd from=0;
 		unsgd to=0;
 		string str;
+		string catchName;
 		regex a("\\s*,\\s*$");
 		regex b("");
+		regex elName("[a-zA-Z]{4,}");
 		bool alreadyExist=false;
 		bool findFrom=false;
 		bool findTo=false;
 		bool uniq=true;
 		out<<"basis=\{"<<endl;
+//TODO надо бы сделать вот что: писать не EN в строках, содержащих названия оболочек и показатели экспонент, а сокращенное название элемента
 	for (typename map< vector< string> , vector < vector < vector < pair < T, T> > > > >::const_iterator it = content.begin(); it != content.end(); it++){ 
 		for ( int i=0; i<int(it->first.size()); i++){
 			if(it->first[i]=="!") out<<'\n';
+			if(regex_search(it->first[i],elName))
+				catchName=it->first[i];
 			out<<' '<<it->first[i];
 		}
 		out<<'\n'; 
 		for (int i=0; i<int(it->second.size()); i++){
 			uniqueIndex.clear();
-			out<<shellName(i)<<" , EN , ";
+			out<<shellName(i)<<" , "<<elementSymbol(catchName)<<" , ";
 			search.clear();
 			for (int j=0; j<int(it->second[i].size()); j++){
 				alreadyExist=false;
@@ -267,13 +379,15 @@ bool SingleBasisSet<T>::exportBasisSetMolproFormat( const char * fileName) const
 	out<<"}"<<endl;
 	out.close();
 	ifstream inp("outputFile");
-	ofstream outGood("goodBasisStrings.basis");
+	ofstream outGood(fileName);
 	while(getline(inp,str)){
 		if(regex_search(str,a)){
 			str=regex_replace(str,a,b);
 		}
 		outGood<<str<<endl;
 	}
+	if(remove("outputFile")!=0)
+		cerr<<"Файл \"outputFile\" не удален!"<< endl;
 	return true;	
 }
 
--- MolproFormat/main.cpp
+++ MolproFormat/main.cpp
@@ -5,9 +5,9 @@ int main(int argc, char** argv){
 	SingleBasisSet<double> A;
 	//if(!A.importBasisSetGamessFormat("HCuN.basis")) return 1;
 	//A.printBasisSet();
-	A.importBasisSetMolproFormat("goodBasisStrings.basis");
-//	A.exportBasisSetMolproFormat("1HNiF.basis");
-	A.printBasisSet();
+	A.importBasisSetMolproFormat("6-311+G.basis");
+	A.exportBasisSetMolproFormat("a.basis");
+//	A.printBasisSet();
 	return 0;
 }
 
